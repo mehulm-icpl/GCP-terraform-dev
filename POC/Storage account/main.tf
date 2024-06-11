a1d005984da96e6bf31ff4991be71e16f275c7e5
mehul-icpl <mehulm@infopercept.com>
2024-06-11 19:07:51 +0530

    Saperated providers.tf and created gitignore

--- POC/Storage account/main.tf
+++ POC/Storage account/main.tf
@@ -1,9 +1,3 @@
-provider "google" {
-  project = "wired-cogency-426104-a5"
-  region  = "us-central1"
-  credentials = file("C:/Users/MehulM/Downloads/wired-cogency-426104-a5-227fb6c443c9.json")
-}
-
 # resource "google_project" "my_project_terraform" {
 #   name       = "my_project_terraform"
 #   project_id = "my-new-project-123"
---
+++ POC/Storage account/providers.tf
@@ -0,0 +1,5 @@
+provider "google" {
+  project = "wired-cogency-426104-a5"
+  region  = "us-central1"
+  credentials = file("C:/Users/MehulM/Downloads/wired-cogency-426104-a5-227fb6c443c9.json")
+}
