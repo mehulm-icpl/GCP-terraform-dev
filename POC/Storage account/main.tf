a6f53b497ca4a28bf09f843a87a53f7ffa496981
mehul-icpl <mehulm@infopercept.com>
2024-06-19 18:40:20 +0530

    added vm creation

--- POC/Storage account/main.tf
+++ POC/Storage account/main.tf
@@ -13,41 +13,69 @@ resource "google_storage_bucket_object" "document" {
 }
 
 # Create a custom IAM role
-resource "google_project_iam_custom_role" "my_custom_role" {
-  role_id     = "myCustomRole_dev"
-  title       = "My Custom Role"
+resource "google_project_iam_custom_role" "terraform_role" {
+  role_id     = "terraform_dev"
+  title       = "Terraform Role dev"
   description = "A custom role for managing resources"
   permissions = [
     "iam.roles.list",
     "storage.buckets.list",
+    "storage.objects.list",
+    "storage.objects.create",
+    "compute.instances.list",
+    "compute.instances.get",
     "resourcemanager.projects.get" # Console Access permission
   ]
   project = "wired-cogency-426104-a5"
 }
 
 # Create a service account
+resource "google_service_account" "my_service_account" {
+  account_id   = "my-service-account"
+  display_name = "terraform service account"
+  project      = "wired-cogency-426104-a5"
+}
 
-# resource "google_service_account" "my_service_account" {
-#   account_id   = "my-service-account"
-#   display_name = "My Service Account"
-#   project      = "wired-cogency-426104-a5"
-# }
+# Bind the custom role to the service account
+resource "google_project_iam_binding" "binding" {
+  project = "wired-cogency-426104-a5"
+  role    = google_project_iam_custom_role.terraform_role.name
 
-# # Bind the custom role to the service account
-# resource "google_project_iam_binding" "binding" {
+  members = [
+    "user:durva.purani17@gmail.com",
+  ]
+}
+
+# Create an IAM user with out creating the service account
+# resource "google_project_iam_member" "my_iam_user" {
 #   project = "wired-cogency-426104-a5"
 #   role    = google_project_iam_custom_role.my_custom_role.name
-
-#   members = [
-#     "serviceAccount:${google_service_account.my_service_account.email}",
-#   ]
+#   member  = "user:mehulmovadiya7@gmail.com" # Replace with the desired email address
 # }
 
-# Assuming you have already configured the required GCP provider settings
 
-# Create an IAM user
-resource "google_project_iam_member" "my_iam_user" {
-  project = "wired-cogency-426104-a5"
-  role    = google_project_iam_custom_role.my_custom_role.name
-  member  = "user:mehulmovadiya7@gmail.com" # Replace with the desired email address
+# creating the vm
+resource "google_compute_instance" "demo-vm" {
+  name         = "demo-vm"
+  machine_type = "n2-standard-2"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_lable = "demo-lable"
+      }
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+  service_account {
+    email  = google_service_account.my_service_account.email
+    scopes = ["cloud-platform"]
+  }
 }
